2be10601b43c0740b5d0a641542f50739b8b381c
Eugene Shangin <eugeneshanginr@gmail.com>
2022-02-17 19:28:58 +0300

    feat: added node server

---
+++ server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.static('dist'))
+
+app.listen(port, () => console.log(`App listening at http://0.0.0.0:${port}`));
